b8d6e4a41e945744c008bc645847756b10a4ec4a
Roberto Sousa <robert6257@gmail.com>
2022-04-16 18:47:39 -0300

    Criando o productIndex

--- GeekShopping.ProductAPI/Controllers/ProductController.cs
+++ GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -35,7 +35,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<ProductVO>> Create(ProductVO vo)
+        public async Task<ActionResult<ProductVO>> Create([FromBody] ProductVO vo)
         {            
             if (vo == null) return BadRequest();
             var product = await _repository.Create(vo);
@@ -44,7 +44,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpPut]
-        public async Task<ActionResult<ProductVO>> Update(ProductVO vo)
+        public async Task<ActionResult<ProductVO>> Update([FromBody] ProductVO vo)
         {            
             if (vo == null) return BadRequest();
             var product = await _repository.Update(vo);
