73e5ae4ed49ddd4d4fbbac2f54773c9d90493ef4
Roberto Sousa <robert6257@gmail.com>
2022-04-16 14:20:50 -0300

    Adicionado a classe MappingConfig

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,6 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
 builder.Services.AddDbContext<MySqlContext>(options =>
     options.UseMySql(connection, new MySqlServerVersion(new Version(8,0,28))));
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
