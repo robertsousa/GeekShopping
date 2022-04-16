a84f459889212b7578a8beea9af505f06031c45f
Roberto Sousa <robert6257@gmail.com>
2022-04-16 17:00:45 -0300

    Criando a Classe HttpClienteExtension

---
+++ GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace GeekShopping.Web.Utils
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> GetTAsync<T>(this HttpResponseMessage response) {
+
+            if (!response.IsSuccessStatusCode) throw new ApplicationException($"Something went wrong calling the API: " +
+                $"{response.ReasonPhrase}");
+
+            var dataAsStrting = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonSerializer.Deserialize<T>(dataAsStrting, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
