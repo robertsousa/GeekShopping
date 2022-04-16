d5e0edf27ef557b1a02d6c769a019d47be88c269
Roberto Sousa <robert6257@gmail.com>
2022-04-16 13:35:42 -0300

    Criado a classe Product e adicionado ao mapeamento

--- GeekShopping.ProductAPI/Model/Context/MySqlContext.cs
+++ GeekShopping.ProductAPI/Model/Context/MySqlContext.cs
@@ -7,5 +7,7 @@ namespace GeekShopping.ProductAPI.Model.Context
         public MySqlContext(){}
         public MySqlContext(DbContextOptions<MySqlContext> options ) : base(options){}
 
+        public DbSet<Product> Products { get; set; }
+
     }
 }
