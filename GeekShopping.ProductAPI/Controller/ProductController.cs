9505a229da7e41b31c5b36c2c7b1002ea634c564
Roberto Sousa <robert6257@gmail.com>
2022-04-24 12:39:33 -0300

    Adicionando a lista de produto na tela principal

--- GeekShopping.ProductAPI/Controller/ProductController.cs
+++ GeekShopping.ProductAPI/Controller/ProductController.cs
@@ -19,7 +19,6 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpGet]
-        [Authorize]
         public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
         {
             var products = await _repository.FindAll();
--- GeekShopping.Web/Controllers/ProductController.cs
+++ GeekShopping.Web/Controllers/ProductController.cs
@@ -18,9 +18,8 @@ namespace GeekShopping.Web.Controllers
 
         [Authorize]
         public async Task<IActionResult> ProductIndex()
-        {
-            var token = await HttpContext.GetTokenAsync("access_token");
-            var products = await  _productService.FindAllProducts(token);
+        {            
+            var products = await  _productService.FindAllProducts("");
             return View(products);
         }
 
