90a7fa943e2655492c805dd86d4a58fd240b882e
Roberto Sousa <robert6257@gmail.com>
2022-04-25 17:37:45 -0300

    Ajuste da tela de listagem, na tela inicial

--- GeekShopping.Web/Models/ProductModel.cs
+++ GeekShopping.Web/Models/ProductModel.cs
@@ -3,10 +3,26 @@
     public class ProductModel
     {
         public long Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public decimal Price { get; set; }
-        public string Description { get; set; }
-        public string CategoryName { get; set; }
-        public string ImageURL { get; set; }
+        public string? Description { get; set; }
+        public string? CategoryName { get; set; }
+        public string? ImageURL { get; set; }
+
+        public string SubstringName()
+        {
+            if (Name!.Length < 24) 
+                return Name;
+
+            return $"{Name.Substring(0, 21)} ...";
+        }
+
+        public string SubstringDescription()
+        {
+            if (Description!.Length < 355) 
+                return Description;
+
+            return $"{Description.Substring(0, 352)} ...";
+        }
     }
 }
