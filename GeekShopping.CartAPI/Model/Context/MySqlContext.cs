f33a5036d1d48b83e560510072ab75c096d4fe68
Roberto Sousa <robert6257@gmail.com>
2022-04-30 17:16:56 -0300

    Configurando o startup e DbContext de CartAPI

---
+++ GeekShopping.CartAPI/Model/Context/MySqlContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CartAPI.Model.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext(){}
+        public MySqlContext(DbContextOptions<MySqlContext> options ) : base(options){}
+
+        public DbSet<Product> Products { get; set; }
+
+        
+
+    }
+}
