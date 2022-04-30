9dbbb22c66bf534a3f570302962fa147e28237e4
Roberto Sousa <robert6257@gmail.com>
2022-04-30 17:37:33 -0300

    Mapenado a entidade do carrinho de compra

---
+++ GeekShopping.CartAPI/Model/CartDetail.cs
@@ -0,0 +1,21 @@
+﻿using GeekShopping.CartAPI.Model.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.CartAPI.Model
+{
+    [Table("cart_detail")]
+    public class CartDetail : BaseEntity
+    {
+        public long CartHeaderId { get; set; }
+
+        [ForeignKey("CartHeaderId")]
+        public CartHeader? CartHeader { get; set; }
+
+        [ForeignKey("ProductId")]
+        public Product? ProductId { get; set; }
+
+        [Column("count")]
+        public int Count { get; set; }
+
+    }
+}
