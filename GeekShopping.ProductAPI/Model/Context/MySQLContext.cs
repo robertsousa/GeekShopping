5d931ddbe00aa6f88e3c930c0ec02635607509a5
Roberto Sousa <robert6257@gmail.com>
2022-10-10 16:56:05 -0300

    Create the class Product

---
+++ GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,17 @@
+﻿using GeekShopping.ProductAPI.Model.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySQLContext: DbContext
+    {
+        public MySQLContext() {}
+
+        public MySQLContext(DbContextOptions<MySQLContext> options ) : base (options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
