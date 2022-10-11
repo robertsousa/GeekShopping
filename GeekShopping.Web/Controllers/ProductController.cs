322cc704221a0ace239ab7642eb103de597091b8
Roberto Sousa <robert6257@gmail.com>
2022-10-11 13:37:28 -0300

    Implementando a tela de Delete de produtos

--- GeekShopping.Web/Controllers/ProductController.cs
+++ GeekShopping.Web/Controllers/ProductController.cs
@@ -54,5 +54,24 @@ namespace GeekShopping.Web.Controllers
             }
             return View(model);
         }
+
+        public async Task<IActionResult> ProductDelete(long id)
+        {
+            var model = await _productService.FindProductById(id);
+            if (model != null) return View(model);
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductDelete(ProductModel model)
+        {
+            
+            var response = await _productService.DeleteProductById(model.Id);
+            if (response != null) return RedirectToAction(nameof(ProductIndex));
+            
+            return View(model);
+        }
+
     }
 }
