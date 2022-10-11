cad464e796285cb9ba90681c302323e5c7bc1faf
Roberto Sousa <robert6257@gmail.com>
2022-10-11 11:37:11 -0300

    Injeção de dependencia da classe ProductService

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -1,6 +1,10 @@
+using GeekShopping.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpClient<IProductService, IProductService>(c => 
+    c.BaseAddress = new Uri(builder.Configuration["ServicesUrls:ProductAPI"]));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
